eb86ed7a29e0757e849a9dbc33b6c48860f4e093
jeongsinae <49273782+jeongsinae@users.noreply.github.com>
2025-08-12 09:25:08 +0900

    Update fan_code.py

--- fan_code.py
+++ fan_code.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import picamera
 
 username = 'ddochi'
-passwd = '~wnajrqkq2704'
+passwd = 'passward'
 
 src = '/home/pi/Desktop/capture/img.jpg'
 dst = '/home/ddochi/Desktop/face/data/result/result/test.jpg'
